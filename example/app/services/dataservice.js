b038d3d0af42c4c363e974de5ba9ff2ef2a0159c
Frank Panetta <entrendipity[at]gmail.com>
2014-04-01 16:12:50 +1100

    change demo data

--- example/app/services/dataservice.js
+++ example/app/services/dataservice.js
@@ -2,7 +2,7 @@
 
 var DataService = {
 	getData: function() {
-		return [{id:'1', firstName:'Frank', lastName: "Smith", gender:'male', dob:'1980-03-03', occupation:'dentist', hobbies: ['reading', 'golf', 'computer programming']},
+		return [{id:'1', firstName:'Frank', lastName: "Smith", gender:'male', dob:'1980-03-03', occupation:'dentist', hobbies: ['reading', 'golfing', 'computer programming']},
 			{id:'2', firstName:'Lisa', lastName: "Jones", gender:'female', dob:'1985-02-22', occupation:'accountant', hobbies: ['reading']},
       { firstName: "John", lastName: "Citizen", gender:'male', dob:'1975-12-11', occupation:'unemployed', hobbies: ['watching TV']}];
 	}
