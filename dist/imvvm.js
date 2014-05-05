99b05030f2f1e75c69ac925bd4b6b5871f554a76
Frank Panetta <entrendipity[at]gmail.com>
2014-05-05 23:30:38 +1000

    remove comment

--- dist/imvvm.js
+++ dist/imvvm.js
@@ -312,7 +312,7 @@ exports.getInitialState = function(appNamespace, domainModel, stateChangedHandle
 	//reinitialize with all data in place
 	for(dataContext in domain){
 		if(domain.hasOwnProperty(dataContext)){
-			appState.state[dataContext] = new dataContexts[dataContext](appState.state[dataContext]/*, true*/);
+			appState.state[dataContext] = new dataContexts[dataContext](appState.state[dataContext]);
     }
   }
 
