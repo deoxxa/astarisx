942c2c6e02bfac103617046021af979159b06d89
Frank Panetta <entrendipity[at]gmail.com>
2014-09-10 20:18:38 +1000

    change test to typeof

--- dist/astarisx.js
+++ dist/astarisx.js
@@ -11,7 +11,7 @@ module.exports = Astarisx;
 // https://github.com/d4tocchini/customevent-polyfill
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
 // modified by fattenap to test if function -> ie 11
-if (!window.CustomEvent || Object.prototype.toString.call(window.CustomEvent) !== '[object Function]') {
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
     var CustomEvent = function(event, params) {
         var evt;
         params = params || {
