bdb0931f491fc2fea475b944513f197ba36da136
Frank Panetta <entrendipity[at]gmail.com>
2014-11-16 12:57:37 +1100

    test mochify with travis

--- test/index.js
+++ test/index.js
@@ -1,3 +1,17 @@
+var React          = require('react');
+var ReactAddons    = require('react/addons'); // You also need to require the addons
+var ReactTestUtils = React.addons.TestUtils;  // <- YEAH!
+var Astarisx = require('../src/core');
+var StateManager = require('../src/stateManager');
+var stateMgr;
+
+// var initializeAppContext = function(options, initCtxObj){
+
+//   stateMgr = new StateManager(, options, initCtxObj);
+//   stateMgr.bind('mockedStateChange', function(appContext) {
+//     console.log('appContext => ', appContext);
+//   });
+// }
 var assert = require("assert")
 describe('Array', function(){
   describe('#indexOf()', function(){
@@ -7,3 +21,37 @@ describe('Array', function(){
     })
   })
 })
+
+// // console.log('TESTING');
+// var page = require('webpage').create();
+// page.open('index.html', function() {
+//   console.log(' 123');
+//   // page.render('example.png');
+//   phantom.exit();
+// });
+
+
+// describe("DOM Tests", function () {
+//     var el = document.createElement("div");
+//     el.id = "myDiv";
+//     el.innerHTML = "Hi there!";
+//     el.style.background = "#ccc";
+//     document.body.appendChild(el);
+ 
+//     var myEl = document.getElementById('myDiv');
+//     it("is in the DOM", function () {
+//         expect(myEl).to.not.equal(null);
+//     });
+ 
+//     it("is a child of the body", function () {
+//         expect(myEl.parentElement).to.equal(document.body);
+//     });
+ 
+//     it("has the right text", function () {
+//         expect(myEl.innerHTML).to.equal("Hi there!");
+//     });
+ 
+//     it("has the right background", function () {
+//         expect(myEl.style.background).to.equal("rgb(204, 204, 204)");
+//     });
+// });
